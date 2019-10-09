9798e79454feb2a332e9cb688e221dd06cb0a1ff
fikario <riocourio@gmail.com>
2019-10-09 17:40:28 +0700

    add login screen

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+import Login from './src/screens/Login'
+import ForYou from './src/screens/ForYou'
+import scratch from './src/screens/scratch'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+
+const AppNavigator = createStackNavigator(
+  {
+    Index: Login,
+    Home : ForYou,
+    scratch : scratch
+  },
+  {
+    initialRouteName: 'Home'
+  }
+)
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default class App extends Component{
+  render() {
+    return (
+      <AppContainer/>
+    )
+  }
+}
---
+++ src/screens/Login.js
@@ -0,0 +1,62 @@
+import React,{Component} from 'react';
+import {Keyboard}from 'react-native';
+import {Button,Input,Item,Text,Container, Content,Form,Label} from 'native-base';
+
+class Login extends Component{
+    constructor(){
+        super();
+        this.state = {
+            emailInput : '',
+            passwordInput : '',
+            data : {email : 'f@gmail.com', password : '12345'},
+            emailRegex : '[a-z0-9]*[a-z0-9\.]+@[a-z0-9]+(\.[a-z0-9]+)*(\.[a-z0-9]+)'
+        }
+    }
+
+    inputChecker=()=>{  
+        Keyboard.dismiss()
+        const email = String(this.state.emailInput).toLowerCase()
+        const password = String(this.state.passwordInput)
+        const regex = this.state.emailRegex
+        let regexResult = email.match(regex)
+        {regexResult==null? regexResult = '' : regexResult = regexResult[0]}
+        if(email!==''){
+            if(password!==''){
+                if(regexResult==email){
+                    if(email == this.state.data.email && password == this.state.data.password){
+                        this.props.navigation.navigate('Home')
+                    }
+                    else console.log(this.state.data.password)
+                }
+                else alert('Invalid Email Syntax')
+            }
+            else alert('Password cannot be blank')
+        }
+    else alert('Email cannot be blank')
+
+        
+    }
+    render(){
+        return(
+            <Container>
+                <Content>
+                    <Text>LOG IN</Text>
+                    <Text>Login with your WEBTOON account</Text>
+                    <Form>
+                        <Item stackedLabel>
+                            <Label>Email</Label>
+                            <Input onChangeText = {(e)=>this.setState({emailInput : e})}/>
+                        </Item>
+                        <Item stackedLabel> 
+                            <Label>Password</Label>
+                            <Input onChangeText = {(e)=>this.setState({passwordInput : e})}/>
+                        </Item>
+                        <Button primary onPress = {()=>this.inputChecker()}><Text>Log In</Text></Button>
+                    </Form>
+                </Content>
+            </Container>
+        )
+    }
+}
+
+export default Login;
